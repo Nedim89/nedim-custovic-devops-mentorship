51febb61b5d5cbf8481cfc245d371a63684fc753
Nedim89 <nedim.custovic@gmail.com>
2023-03-21 10:00:51 +0000

    Added scripts for Chapter13 and some scripts inside

---
+++ week-3/chapter13/test1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for test in Sarajevo Zenica Tuzla Konjic Mostar BanjaLuka
+do
+ echo The next state is $test
+done
---
+++ week-3/chapter13/test1b.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for test in Zenica Sarajevo Mostar Tuzla
+do
+ echo "The next state is $test"
+done
+echo "The last state we visited was $test"
+test=Zagreb
+echo "Wait, now we're visiting $test"
